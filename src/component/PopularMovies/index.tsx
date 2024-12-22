64276f1154ea38cdd427c2f0002c83f5b38d3ebd
SaraMecinovic1 <saramecinovic87@gmail.com>
2024-12-22 17:12:22 +0100

    fixed some on popular movies

--- src/component/PopularMovies/index.tsx
+++ src/component/PopularMovies/index.tsx
@@ -18,9 +18,9 @@ function PopularMovies() {
   }, []);
 
   return (
-    <div className="w-full h-auto mt-7 ">
+    <div className="w-full h-auto mt-7">
       <div className="text-center px-4 mb-10">
-        <h1 className="text-4xl font-bold font-inter text-[#cfcccc] ">
+        <h1 className="text-4xl font-bold font-inter text-[#cfcccc]">
           WE RECOMMEND
         </h1>
         <p className="text-lg font-medium italic text-secondary opacity-80">
@@ -29,19 +29,22 @@ function PopularMovies() {
       </div>
 
       {!isLoading ? (
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 gap-6 px-4">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 px-4">
           {movies.map((movie) => (
-            <div key={movie.id} className=" overflow-hidden shadow-lg mb-3">
+            <div
+              key={movie.id}
+              className="shadow-lg rounded-md overflow-hidden flex flex-col"
+            >
               <img
                 src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                 alt={movie.title}
                 className="w-full h-auto"
               />
-              <div className="p-3 flex justify-between items-center ">
-                <h3 className="text-l font-medium text-[#c9c7c7] mr-4">
+              <div className="p-3 flex flex-col sm:flex-row justify-between items-start sm:items-center">
+                <h3 className="text-sm font-medium text-[#c9c7c7] mr-4 line-clamp-2">
                   {movie.title.toUpperCase()}
                 </h3>
-                <p className="flex items-center mt-1 text-[#c9c7c7]">
+                <p className="flex items-center text-sm mt-2 sm:mt-0 text-[#c9c7c7]">
                   <StarIcon
                     width={17}
                     className="text-yellow-600 mb-0.5 mr-1"
