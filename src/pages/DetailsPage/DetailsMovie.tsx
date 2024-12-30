ba6b589f68ec7fcb4e04577668a209c228eef8b4
SaraMecinovic1 <saramecinovic87@gmail.com>
2024-12-30 19:30:05 +0100

    details page is done

--- src/pages/DetailsPage/DetailsMovie.tsx
+++ src/pages/DetailsPage/DetailsMovie.tsx
@@ -43,42 +43,15 @@ export default function DetailsMovie() {
   return (
     <div className="w-full h-auto text-accent">
       <div className="w-full h-auto mt-20 lg:p-10 sm:p-5">
-        <div
-          className="
-            items-center
-            flex flex-col gap-5 pt-5
-            sm:p-5
-            sm:mt-5
-            md:flex-row md:gap-2 md:items-start md:mt-10
-            lg:mt-0
-          "
-        >
-        <img
-            src={https://image.tmdb.org/t/p/w500${movie.poster_path}}
-              alt={data?.title}
-            className="
-              rounded-lg border
-              w-[330px] h-[440px]
-              sm:w-[400px] sm:h-[550px] sm:ml-[37px]
-              md:w-[400px] md:h-[550px]
-              lg:w-[400px] lg:h-[550px]
-              object-cover
-            "
+        <div className="items-center flex flex-col gap-5 pt-5 sm:p-5 sm:mt-5 md:flex-row md:gap-2 md:items-start md:mt-10 lg:mt-0">
+          <img
+            src={`https://image.tmdb.org/t/p/w500${data?.poster_path}`}
             alt={data?.title}
+            className="rounded-lg border w-[330px] h-[440px] sm:w-[400px] sm:h-[550px] sm:ml-[37px] md:w-[400px] md:h-[550px] lg:w-[400px] lg:h-[550px] object-cover"
           />
 
-<div
-            className="
-              flex flex-col justify-center gap-1
-              md:gap-1
-              md:items-start
-              md:mx-5
-              sm:px-5 lg:px-5
-              sm:py-0 lg:py-0
-              items-center sm:items-center
-            "
-          >         
-          <h1 className="text-3xl sm:text-3xl lg:text-4xl font-bold text-accent mb-2">
+          <div className="flex flex-col justify-center gap-1 md:gap-1 md:items-start md:mx-5 sm:px-5 lg:px-5 sm:py-0 lg:py-0 items-center sm:items-center">
+            <h1 className="text-3xl sm:text-3xl lg:text-4xl font-bold text-accent mb-2">
               {data?.title.toUpperCase()}
             </h1>
             <h2 className="text-xl sm:text-xl lg:text-2xl font-normal text-accent">
@@ -93,29 +66,20 @@ export default function DetailsMovie() {
                   width={24}
                   height={24}
                   className="text-secondary mr-1"
-                />) : (
-                  <FaHeart
-                    onClick={toggleWatchList}
-                    fontSize={20}
-                    width={24}
-                    height={24}
-                    className="text-secondary mr-1"
-                  />
-                )}
-                ADD TO WATCH LIST
-              </p>
+                />
+              ) : (
+                <FaHeart
+                  onClick={toggleWatchList}
+                  fontSize={20}
+                  width={24}
+                  height={24}
+                  className="text-secondary mr-1"
+                />
+              )}
+              ADD TO WATCH LIST
+            </p>
 
-              <div
-              className="
-                px-5
-                mb-5
-                text-lg w-full
-                sm:w-full lg:w-[500px]
-                md:w-full
-                h-auto mt-4 sm:mt-3 md:mt-3
-                text-center md:text-left md:px-0
-              "
-            >
+            <div className="px-5 mb-5 text-lg w-full sm:w-full lg:w-[500px] md:w-full h-auto mt-4 sm:mt-3 md:mt-3 text-center md:text-left md:px-0">
               {data?.overview}
             </div>
             <div className="mb-10">
@@ -129,6 +93,4 @@ export default function DetailsMovie() {
       </div>
     </div>
   );
-}
-
 }
