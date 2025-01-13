eea5ca3f5b4255619f194f8aed18044e3556d181
SaraMecinovic1 <saramecinovic87@gmail.com>
2025-01-13 11:56:08 +0100

    added layout

--- src/pages/MyListPage/index.tsx
+++ src/pages/MyListPage/index.tsx
@@ -9,24 +9,23 @@ const MyWatchingList = () => {
 
   useEffect(() => {
     loadWatchlist();
-    console.log("loadWatchlist- ", watchlist);
   }, [loadWatchlist]);
 
   return (
-    <div className="w-full min-h-screen flex flex-col items-center mt-[110px]">
+    <div className="w-full min-h-screen flex flex-col items-center mt-[110px] px-5">
       {watchlist.length === 0 ? (
-        <div className="text-center text-2xl mt-10 text-[27px] text-gray-400">
+        <div className="text-center text-lg sm:text-xl mt-10 text-gray-400">
           🎬 Your watchlist is empty! <br />
           <span
             onClick={() => navigate("/movies")}
-            className="text-secondary font-medium cursor-pointer  italic text-[22px]  opacity-70"
+            className="text-secondary font-medium cursor-pointer italic text-base sm:text-lg opacity-70"
           >
             Start adding your favorite movies now
           </span>
         </div>
       ) : (
-        <div className="w-full mx-10 flex flex-col items-center">
-          <div className="text-center mb-10 text-4xl font-bold font-inter text-secondary">
+        <div className="w-full flex flex-col items-center">
+          <div className="text-center mb-10 text-2xl sm:text-3xl font-bold font-inter text-secondary">
             <h1>MY WATCH LIST 🤍</h1>
           </div>
           {watchlist.map(
@@ -35,17 +34,17 @@ const MyWatchingList = () => {
                 <div
                   onClick={() => navigate(`/movie/${movie.id}`)}
                   key={movie.id}
-                  className="flex flex-row w-[600px] h-[210px] mb-10 bg-[#2e2e2e] rounded-lg"
+                  className="flex flex-row sm:w-[600px] w-full h-[230px] sm:h-[210px] mb-6 bg-[#2e2e2e] rounded-lg overflow-hidden"
                 >
-                  <div className="w-[200px]">
+                  <div className="w-[270px] h-full sm:w-[200px]">
                     <img
                       src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
                       alt={movie.title}
-                      className="w-full h-auto rounded-lg"
+                      className="w-full h-full rounded-t-lg sm:rounded-t-none sm:rounded-l-lg"
                     />
                   </div>
-                  <div className="w-full m-5">
-                    <h1 className="text-xl sm:text-xl lg:text-2xl font-bold text-accent mb-2 overflow-hidden">
+                  <div className="w-full p-4">
+                    <h1 className="text-lg sm:text-xl lg:text-2xl font-bold text-accent mb-2 overflow-hidden">
                       {movie.title?.toUpperCase()}
                     </h1>
                     <p className="text-gray-300 text-sm leading-relaxed line-clamp-5">
