3f86dfb5835080aa56b8e2158a160f7b1c7ec4a9
SaraMecinovic1 <saramecinovic87@gmail.com>
2025-01-17 15:08:16 +0100

    ui for seats page done

--- src/component/Seats.tsx
+++ src/component/Seats.tsx
@@ -3,90 +3,26 @@ import { faSquare } from "@fortawesome/free-solid-svg-icons";
 
 const SeatsBox = () => {
   return (
-    <div className="w-[600px] mr-10 h-auto flex flex-col px-10 ">
-      <div className="flex flex-row justify-center mb-4">
-        <span className="mr-2 text-lg text-accent font-medium px-5">1.</span>
-        {[...Array(6)].map((_, index) => (
-          <FontAwesomeIcon
-            key={index}
-            icon={faSquare}
-            className="text-2xl mx-2 text-accent"
-            id={`seat-1-${index}`}
-          />
-        ))}
-      </div>
-
-      <div className="flex flex-row justify-center mb-4">
-        <span className="mr-2 text-lg font-medium  text-accent px-5 ">2.</span>
-        {[...Array(7)].map((_, index) => (
-          <FontAwesomeIcon
-            key={index}
-            icon={faSquare}
-            className="text-2xl mx-2 text-accent"
-            id={`seat-2-${index}`}
-          />
-        ))}
-      </div>
-
-      <div className="flex flex-row justify-center mb-4">
-        <span className="mr-2 text-lg font-medium  text-accent px-5">3.</span>
-        {[...Array(8)].map((_, index) => (
-          <FontAwesomeIcon
-            key={index}
-            icon={faSquare}
-            className="text-2xl mx-2 text-accent"
-            id={`seat-3-${index}`}
-          />
-        ))}
-      </div>
-
-      <div className="flex flex-row justify-center mb-4">
-        <span className="mr-2 text-lg font-medium  text-accent px-5">4.</span>
-        {[...Array(9)].map((_, index) => (
-          <FontAwesomeIcon
-            key={index}
-            icon={faSquare}
-            className="text-2xl mx-2 text-accent"
-            id={`seat-4-${index}`}
-          />
-        ))}
-      </div>
-
-      <div className="flex flex-row justify-center mb-4">
-        <span className="mr-2 text-lg font-medium  text-accent px-5">5.</span>
-        {[...Array(10)].map((_, index) => (
-          <FontAwesomeIcon
-            key={index}
-            icon={faSquare}
-            className="text-2xl mx-2 text-accent"
-            id={`seat-5-${index}`}
-          />
-        ))}
-      </div>
-
-      <div className="flex flex-row justify-center mb-4">
-        <span className="mr-2 text-lg font-medium  text-accent px-5">6.</span>
-        {[...Array(11)].map((_, index) => (
-          <FontAwesomeIcon
-            key={index}
-            icon={faSquare}
-            className="text-2xl mx-2 text-accent"
-            id={`seat-6-${index}`}
-          />
-        ))}
-      </div>
-
-      <div className="flex flex-row justify-center mb-4">
-        <span className="mr-2 text-lg font-medium  text-accent px-5">7.</span>
-        {[...Array(12)].map((_, index) => (
-          <FontAwesomeIcon
-            key={index}
-            icon={faSquare}
-            className="text-2xl mx-2 text-accent"
-            id={`seat-7-${index}`}
-          />
-        ))}
-      </div>
+    <div className="w-full max-w-[400px] sm:max-w-[600px] lg:max-w-[800px] mx-auto h-auto flex flex-col px-5 sm:px-6 lg:px-8">
+      {[...Array(7)].map((_, rowIndex) => {
+        // Array(7) je mojih 7 redova
+        const seatCount = rowIndex + 6; //seatCount-broj sedišta u svakom redu,1.red 0+6,i za svaki ostali index+6 ,tj za pojedno mesto vise u svakom sl redu
+        return (
+          <div key={rowIndex} className="flex flex-row justify-center mb-4">
+            <span className="mr-2 text-sm sm:text-lg font-medium text-accent px-3">
+              {rowIndex + 1}.
+            </span>
+            {[...Array(seatCount)].map((_, seatIndex) => (
+              <FontAwesomeIcon
+                key={seatIndex}
+                icon={faSquare}
+                className="text-lg sm:text-2xl mx-1 sm:mx-2 text-accent"
+                id={`seat-${rowIndex + 1}-${seatIndex}`}
+              />
+            ))}
+          </div>
+        );
+      })}
     </div>
   );
 };
--- src/pages/Reservation/BookSeats.tsx
+++ src/pages/Reservation/BookSeats.tsx
@@ -1,40 +1,47 @@
-import Loader from "@/component/loading";
 import SeatsBox from "@/component/Seats";
 import Stepper from "@/component/Stepper";
 import { faSquare } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSquareXmark } from "@fortawesome/free-solid-svg-icons";
+import { Button } from "@/components/ui/button";
 
-type Props = {};
-
-const Seats = (props: Props) => {
+const Seats = () => {
   return (
-    <div className="w-full h-full px-4 sm:px-6 lg:px-10">
+    <div className="w-full h-full px-4 sm:px-6 lg:px-8">
       <Stepper />
 
-      <div className="w-full h-auto mb-5 sm:mb-20 p-3 sm:p-7 rounded-lg bg-[#2e2e2e] flex flex-col items-center gap-5">
-        <div className="mb-">
-          <h1 className="font-normal text-xl text-accent">SCREEN</h1>
+      <div className="w-full h-auto mb-5 p-3 sm:p-6 lg:p-8 rounded-lg bg-[#2e2e2e] flex flex-col items-center gap-2">
+        <div className="mb-4">
+          <h1 className="font-normal text-lg sm:text-xl text-accent">SCREEN</h1>
         </div>
 
         <SeatsBox />
-        <hr className=" border-gray-700 w-full  " />
+        <hr className="border-gray-700 w-full" />
         <div className="w-full flex flex-row justify-center">
-          <div className="mx-3">
+          <div className="mx-2 sm:mx-3">
             <FontAwesomeIcon
               icon={faSquare}
-              className="text-xl mx-2 text-accent"
+              className="text-lg sm:text-xl mx-2 text-accent"
             />
-            <p className="text-sm text-textColor">FREE</p>
+            <p className="text-xs sm:text-sm text-textColor">FREE</p>
           </div>
-          <div className="mx-3 text-center">
+          <div className="mx-2 sm:mx-3 text-center">
             <FontAwesomeIcon
               icon={faSquareXmark}
-              className="text-xl mx-2 text-accent"
+              className="text-lg sm:text-xl mx-2 text-accent"
             />
-            <p className="text-sm text-textColor">CHOSEN</p>
+            <p className="text-xs sm:text-sm text-textColor">CHOSEN</p>
           </div>
         </div>
+      </div>
+
+      <div className="flex justify-center">
+        <Button
+          type="submit"
+          className="w-40 sm:w-40 lg:w-48 text-accent rounded-3xl bg-secondary hover:bg-orange-600"
+        >
+          NEXT STEP
+        </Button>
       </div>
     </div>
   );
