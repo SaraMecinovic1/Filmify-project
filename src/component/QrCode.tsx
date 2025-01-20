b740204faff6368c6564e264bf49f9da26c5b53f
SaraMecinovic1 <saramecinovic87@gmail.com>
2025-01-20 13:38:49 +0100

    qr ui done

--- src/component/QrCode.tsx
+++ src/component/QrCode.tsx
@@ -4,13 +4,20 @@ const QRCodeGenerator = ({ reservationId }: { reservationId: string }) => {
   const qrData = `http://localhost:5173/your-qr/${reservationId}`; // Za lokalni razvoj
 
   return (
-    <div className="flex justify-center items-center flex-col">
-      <h2 className="text-2xl font-semibold mb-3">YOUR QR CODE 📲</h2>
+    <div className="flex justify-center text-center items-center flex-col">
+      <h2 className="text-3xl font-semibold mb-4 text-secondary">
+        QR CODE FOR YOUR RESERVATION 📲
+      </h2>
       <QRCode
         value={qrData} // Podaci koje QR kod treba da sadrži
         size={200}
         fgColor="#000000" // Boja QR koda
       />
+
+      <h1 className="font-medium text-lg text-textColor mt-4">
+        Thank you for choosing us,{" "}
+        <span className="text-secondary font-medium">Filmify</span>!
+      </h1>
     </div>
   );
 };
--- src/pages/Reservation/Book.tsx
+++ src/pages/Reservation/Book.tsx
@@ -36,59 +36,60 @@ const Book = () => {
     <div className="w-full h-full px-6 flex flex-col items-center justify-center text-accent">
       <Stepper />
       <div className="w-full max-w-md sm:max-w-lg p-8 bg-[#2e2e2e] rounded-lg shadow-lg flex flex-col items-center gap-6">
-        <h2 className="text-2xl font-semibold mb-3">BOOKING DETAILS 🧾</h2>
-        <div className="w-full flex justify-center text-center space-y-2">
-          <div className="text-left space-y-2">
-            <p className="text-lg text-textColor">
-              Movie:
-              <span className="font-semibold ml-2 text-lg italic text-white">
-                {userData?.movieTitle}
-              </span>
-            </p>
-            <p className="text-lg text-textColor">
-              Tickets:
-              <span className="font-semibold ml-2 italic text-lg text-white">
-                {totalTickets}
-              </span>
-            </p>
-            <p className="text-lg text-textColor">
-              Date:
-              <span className="font-semibold ml-2 italic text-lg text-white">
-                {userData?.date}
-              </span>
-            </p>
-            <p className="text-lg text-textColor">
-              Time:
-              <span className="font-semibold ml-2 italic text-lg text-white">
-                20:00 PM
-              </span>
-            </p>
-            <p className="text-lg text-textColor">
-              Seats:
-              <span className="font-semibold ml-2 italic text-lg text-white">
-                {userData?.seats?.join(", ")}
-              </span>
-            </p>
-          </div>
-        </div>
-
-        <div className="mt-6 w-full flex justify-center">
-          <Button
-            onClick={onSubmit}
-            className="w-60 bg-secondary hover:bg-orange-600 text-white py-2 rounded-lg"
-            disabled={loading || reservationSent}
-          >
-            {reservationSent
-              ? "RESERVATION SENT"
-              : loading
-              ? "SENDING..."
-              : "SEND RESERVATION"}
-          </Button>
-        </div>
-
-        {/* Prikazivanje QR koda ako je rezervacija poslata */}
-        {reservationSent && reservationId && (
+        {reservationSent && reservationId ? (
           <QRCodeGenerator reservationId={reservationId} />
+        ) : (
+          <>
+            <h2 className="text-2xl font-semibold mb-3">BOOKING DETAILS 🧾</h2>
+            <div className="w-full flex justify-center text-center space-y-2">
+              <div className="text-left space-y-2">
+                <p className="text-lg text-textColor">
+                  Movie:
+                  <span className="font-semibold ml-2 text-lg italic text-white">
+                    {userData?.movieTitle}
+                  </span>
+                </p>
+                <p className="text-lg text-textColor">
+                  Tickets:
+                  <span className="font-semibold ml-2 italic text-lg text-white">
+                    {totalTickets}
+                  </span>
+                </p>
+                <p className="text-lg text-textColor">
+                  Date:
+                  <span className="font-semibold ml-2 italic text-lg text-white">
+                    {userData?.date}
+                  </span>
+                </p>
+                <p className="text-lg text-textColor">
+                  Time:
+                  <span className="font-semibold ml-2 italic text-lg text-white">
+                    20:00 PM
+                  </span>
+                </p>
+                <p className="text-lg text-textColor">
+                  Seats:
+                  <span className="font-semibold ml-2 italic text-lg text-white">
+                    {userData?.seats?.join(", ")}
+                  </span>
+                </p>
+              </div>
+            </div>
+
+            <div className="mt-6 w-full flex justify-center">
+              <Button
+                onClick={onSubmit}
+                className="w-60 bg-secondary hover:bg-orange-600 text-white py-2 rounded-lg"
+                disabled={loading || reservationSent}
+              >
+                {reservationSent
+                  ? "RESERVATION SENT"
+                  : loading
+                  ? "SENDING..."
+                  : "SEND RESERVATION"}
+              </Button>
+            </div>
+          </>
         )}
       </div>
     </div>
