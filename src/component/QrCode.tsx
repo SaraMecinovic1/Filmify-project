28b9d336adc257654c471d11cca432d71defa52d
SaraMecinovic1 <saramecinovic87@gmail.com>
2025-01-20 15:12:33 +0100

    made ui for reservation details

--- src/component/QrCode.tsx
+++ src/component/QrCode.tsx
@@ -1,22 +1,25 @@
 import QRCode from "react-qr-code";
+import { useNavigate } from "react-router-dom";
 
 const QRCodeGenerator = ({ reservationId }: { reservationId: string }) => {
-  const qrData = `http://localhost:5173/your-qr/${reservationId}`; // Za lokalni razvoj
+  const navigate = useNavigate();
+  const qrData = `http://localhost:5173/your-qr/${reservationId}`;
 
   return (
     <div className="flex justify-center text-center items-center flex-col">
-      <h2 className="text-3xl font-semibold mb-4 text-secondary">
+      <h2 className="text-2xl font-semibold mb-4 text-secondary">
         QR CODE FOR YOUR RESERVATION 📲
       </h2>
       <QRCode
-        value={qrData} // Podaci koje QR kod treba da sadrži
+        onClick={() => navigate(`/your-qr/${reservationId}`)}
+        value={qrData}
         size={200}
-        fgColor="#000000" // Boja QR koda
+        fgColor="#000000"
       />
 
       <h1 className="font-medium text-lg text-textColor mt-4">
         Thank you for choosing us,{" "}
-        <span className="text-secondary font-medium">Filmify</span>!
+        <span className="text-secondary font-medium">FILMIFY</span> !
       </h1>
     </div>
   );
--- src/pages/InfoPage/ReservationInfo.tsx
+++ src/pages/InfoPage/ReservationInfo.tsx
@@ -1,7 +1,55 @@
-import React from "react";
-
 const ReservationInfo = () => {
-  return <div>ReservationInfo</div>;
+  return (
+    <div className="w-full h-[50vh] px-6 flex flex-col items-center justify-center text-textColor">
+      <div className="w-full max-w-xl sm:max-w-lg h-auto text-center mb-7">
+        <h1 className="font-semibold text-3xl mb-4 text-secondary">
+          Hi Sara !
+        </h1>
+        <p className="font-normal  text-xl">
+          Congratulations on creating a movie reservation! Your reservation id
+          is <span className="font-bold">02432543w45312wr</span>. If you need a
+          help, write to <span className="font-semibold">info@filmify.com</span>
+        </p>
+      </div>
+      <div className="w-full max-w-xl sm:max-w-lg p-8 bg-[#2e2e2e] rounded-lg shadow-lg flex flex-col items-center gap-6  ">
+        <h2 className="text-2xl font-semibold mb-3"> RESERVATION DETAILS </h2>
+        <div className="w-full flex justify-center text-center space-y-2">
+          <div className="text-left space-y-2">
+            <p className="text-lg text-textColor">
+              Movie:
+              <span className="font-semibold ml-2 text-lg italic text-white">
+                {userData?.movieTitle}
+              </span>
+            </p>
+            <p className="text-lg text-textColor">
+              Tickets:
+              <span className="font-semibold ml-2 italic text-lg text-white">
+                {totalTickets}
+              </span>
+            </p>
+            <p className="text-lg text-textColor">
+              Date:
+              <span className="font-semibold ml-2 italic text-lg text-white">
+                {userData?.date}
+              </span>
+            </p>
+            <p className="text-lg text-textColor">
+              Time:
+              <span className="font-semibold ml-2 italic text-lg text-white">
+                20:00 PM
+              </span>
+            </p>
+            <p className="text-lg text-textColor">
+              Seats:
+              <span className="font-semibold ml-2 italic text-lg text-white">
+                {userData?.seats?.join(", ")}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ReservationInfo;
